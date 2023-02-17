73badd12be78b1b0ddf3420869e3b8d2ada315d6
sonjiyong08 <sonjiyong08@naver.com>
2023-02-17 17:59:31 +0900

    문자텍스트 수정

--- app/src/main/java/com/example/listscreen/CameraActivity.java
+++ app/src/main/java/com/example/listscreen/CameraActivity.java
@@ -10,6 +10,5 @@ public class CameraActivity extends AppCompatActivity {
     protected  void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cameraactivity_main);
-
     }
 }
--- app/src/main/java/com/example/listscreen/SettingActivity.java
+++ app/src/main/java/com/example/listscreen/SettingActivity.java
@@ -1,15 +1,58 @@
 package com.example.listscreen;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
+    Button btn_dialog;
+    TextView tv_result;
     @Override
-    protected  void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settingactivity_main);
+
+        btn_dialog = (Button)findViewById(R.id.btn_dialog);
+        tv_result = (TextView)findViewById(R.id.tv_content);
+
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(SettingActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("사용자 문자 설정");
+                ad.setMessage("문자를 입력하세요");
+
+
+                final EditText et = new EditText(SettingActivity.this);
+                /// dialog 안에다가 et 객체를 view로 추가하기
+                ad.setView(et);
+
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String result = et.getText().toString();
+                        tv_result.setText(result);
+                        dialog.dismiss();
+                    }
+                });
+
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                ad.show();
+            }
+        });
 
     }
 }

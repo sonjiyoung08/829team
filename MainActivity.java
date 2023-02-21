97a61054d295f55327eb04acc718260e9f1ea713
Lim-Soo-Min <75859938+Lim-Soo-Min@users.noreply.github.com>
2023-02-22 06:39:09 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.team829;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView sign;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //회원가입 버튼
+        sign = findViewById(R.id.signin);
+
+        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
+        sign.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Signup.class);
+            startActivity(intent);
+        });
+    }
+}
